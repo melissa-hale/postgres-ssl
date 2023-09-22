38b019699ae62061bcbe729681cd042650501e03
Melissa Hale <melissakayh@gmail.com>
2023-09-22 15:23:35 -0600

    please

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Set PGPORT to 5432
+export PGPORT=5432
+
+# Call the original entrypoint script
+exec /usr/local/bin/docker-entrypoint.sh "$@"
+
+# Set PGPORT to Proxy Port
+export PGPORT=$TCP_PROXY_PORT
