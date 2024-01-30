acad92a439dcbb02d183dfe68eae3c8614966b2f
Melissa Hale <melissakayh@gmail.com>
2024-01-30 17:11:39 -0600

    update

--- configure-postgres.sh
+++ configure-postgres.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Set default values for environment variables if they are not set
-: ${MAX_CONNECTIONS:=150}
-: ${SHARED_BUFFERS:="128MB"}
+: ${POSTGRES_MAX_CONNECTIONS:=150}
+: ${POSTGRES_SHARED_BUFFERS:="128MB"}
 : ${CONNECTION_CHECK_INTERVAL:=0}
 
 # Use SQL commands to set the configurations
 psql -U postgres -c "ALTER SYSTEM SET client_connection_check_interval = ${CONNECTION_CHECK_INTERVAL};"
-psql -U postgres -c "ALTER SYSTEM SET max_connections = ${MAX_CONNECTIONS};"
-psql -U postgres -c "ALTER SYSTEM SET shared_buffers = '${SHARED_BUFFERS}';"
+psql -U postgres -c "ALTER SYSTEM SET max_connections = ${POSTGRES_MAX_CONNECTIONS};"
+psql -U postgres -c "ALTER SYSTEM SET shared_buffers = '${POSTGRES_SHARED_BUFFERS}';"
 
 # Reload the PostgreSQL configuration
 psql -U postgres -c "SELECT pg_reload_conf();"
