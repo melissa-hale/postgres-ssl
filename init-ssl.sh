8852400f078de377567fcebb4d1a72d2a733f06b
Melissa Hale <melissakayh@gmail.com>
2024-05-30 13:46:24 -0500

    Update init-ssl.sh

--- init-ssl.sh
+++ init-ssl.sh
@@ -9,32 +9,29 @@ sudo mkdir -p "$SSL_DIR"
 # Use sudo to change ownership as root
 sudo chown postgres:postgres "$SSL_DIR"
 
-# Check if certificates already exist
-if [ "$REGENERATE_CERTS" = "true" ] || [ ! -f "$SSL_DIR/server.key" ] || [ ! -f "$SSL_DIR/server.crt" ] || [ ! -f "$SSL_DIR/root.crt" ]; then
-    # Generate Root CA
-    openssl req \
-        -new -x509 -days "${SSL_CERT_DAYS:-820}" -nodes -text -out "$SSL_DIR/root.crt" \
-        -keyout "$SSL_DIR/root.key" -subj "/CN=root-ca" 
-        #-extensions v3_ca \
-        #-config "$SSL_CONF_DIR/ssl-extensions.conf"
+# Generate Root CA
+openssl req \
+    -new -x509 -days "${SSL_CERT_DAYS:-820}" -nodes -text -out "$SSL_DIR/root.crt" \
+    -keyout "$SSL_DIR/root.key" -subj "/CN=root-ca" 
+    #-extensions v3_ca \
+    #-config "$SSL_CONF_DIR/ssl-extensions.conf"
 
-    # Generate Server Certificates
-    openssl req \
-        -new -nodes -text -out "$SSL_DIR/server.csr" \
-        -keyout "$SSL_DIR/server.key" -subj "/CN=localhost"
+# Generate Server Certificates
+openssl req \
+    -new -nodes -text -out "$SSL_DIR/server.csr" \
+    -keyout "$SSL_DIR/server.key" -subj "/CN=localhost"
     
-    openssl x509 \
-        -req -in "$SSL_DIR/server.csr" -text \
-        -out "$SSL_DIR/server.crt" \
-        -CA "$SSL_DIR/root.crt" \
-        -CAkey "$SSL_DIR/root.key" \
-        -CAcreateserial -days "${SSL_CERT_DAYS:-820}"
+openssl x509 \
+    -req -in "$SSL_DIR/server.csr" -text \
+    -out "$SSL_DIR/server.crt" \
+    -CA "$SSL_DIR/root.crt" \
+    -CAkey "$SSL_DIR/root.key" \
+    -CAcreateserial -days "${SSL_CERT_DAYS:-820}"
         # -extfile "$SSL_CONF_DIR/ssl-extensions.conf" \
         # -extensions v3_server_cert 
 
-    chown postgres:postgres "$SSL_DIR/server.key"
-    chmod 600 "$SSL_DIR/server.key"
-fi
+chown postgres:postgres "$SSL_DIR/server.key"
+chmod 600 "$SSL_DIR/server.key"
 
 # PostgreSQL configuration
 cat >> "$PGDATA/postgresql.conf" <<EOF
