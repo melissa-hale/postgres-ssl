842c22e8db094bdd36efe099b80146453c9a3478
Melissa Hale <melissakayh@gmail.com>
2024-05-30 13:44:35 -0500

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# unset PGHOST to force psql to use Unix socket path
-# this is specific to Railway and allows
-# us to use PGHOST after the init
+# Ensure necessary environment variables are available
+SSL_DIR="/var/lib/postgresql/data/certs"
+INIT_SSL_SCRIPT="/docker-entrypoint-initdb.d/init-ssl.sh"
+
+# Check if certificates need to be regenerated
+if [ "$REGENERATE_CERTS" = "true" ] || [ ! -f "$SSL_DIR/server.key" ] || [ ! -f "$SSL_DIR/server.crt" ] || [ ! -f "$SSL_DIR/root.crt" ]; then
+    echo "Running init-ssl.sh to generate new certificates..."
+    bash "$INIT_SSL_SCRIPT"
+else
+    echo "Certificates already exist and REGENERATE_CERTS is not set to true. Skipping certificate generation."
+fi
+
+# Unset PGHOST and PGPORT to force psql to use Unix socket path
 unset PGHOST
 unset PGPORT
 
-# Call the entrypoint script with the
-# approriate PGHOST
-/usr/local/bin/docker-entrypoint.sh "$@"
+# Call the entrypoint script with the appropriate arguments
+exec /usr/local/bin/docker-entrypoint.sh "$@"
